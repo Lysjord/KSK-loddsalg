2243cb1d096c2a1b975ef1026fdc51a41f6e211e
Lysjord <tomasfremnesvik@gmail.com>
2025-05-05 20:19:25 +0200

    Update firebase-config.js
    2.2

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
-// Firebase config placeholder
+// Sett inn din Firebase-konfigurasjon under.
+// Du finner den i Firebase Console > Innstillinger > Prosjektinnstillinger > "SDK setup and config".
+
+// Eksempel:
+const firebaseConfig = {
+  apiKey: "DIN_API_KEY",
+  authDomain: "DITT_PROSJEKT.firebaseapp.com",
+  projectId: "DITT_PROSJEKT_ID",
+  storageBucket: "DITT_PROSJEKT.appspot.com",
+  messagingSenderId: "XXXXXX",
+  appId: "1:XXXXXXX:web:XXXXXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
