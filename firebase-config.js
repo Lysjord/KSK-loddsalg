e1489d9ca5dbe643cfbe7ae8e744e0a22c8302a5
Lysjord <tomasfremnesvik@gmail.com>
2025-05-05 23:41:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 // Sett inn din Firebase-konfigurasjon under.
 // Du finner den i Firebase Console > Innstillinger > Prosjektinnstillinger > "SDK setup and config".
 
-// Eksempel:
-const firebaseConfig = {
-  apiKey: "DIN_API_KEY",
-  authDomain: "DITT_PROSJEKT.firebaseapp.com",
-  projectId: "DITT_PROSJEKT_ID",
-  storageBucket: "DITT_PROSJEKT.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "1:XXXXXXX:web:XXXXXXX"
+// const firebaseConfig = {
+  apiKey: "AIzaSyApTliK7E_6euJJWFeVnAxqlbBK12Ir7Pc",
+  authDomain: "ksk-loddsalg.firebaseapp.com",
+  projectId: "ksk-loddsalg",
+  storageBucket: "ksk-loddsalg.firebasestorage.app",
+  messagingSenderId: "535132446143",
+  appId: "1:535132446143:web:820bc7d375eedeb7ed150a",
+  measurementId: "G-XNE8EJX4GT"
 };
 
 firebase.initializeApp(firebaseConfig);
